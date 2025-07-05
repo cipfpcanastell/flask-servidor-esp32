ddb0abb5c7faf7eea057c05bb55a4c0937619b09
cipfpcanastell <carlos.hernandez.cipfpcanastell@gmail.com>
2025-07-05 08:34:38 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/datos', methods=['POST'])
+def recibir_datos():
+    datos = request.get_json()
+    print("Datos recibidos:", datos)
+    return jsonify({"mensaje": "Datos recibidos correctamente"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
